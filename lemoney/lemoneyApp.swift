aa69402199f4fa6802294a97ee9c4bcb869d5f63
dishita bharadia <dishita.bharadia@gmail.com>
2022-10-22 09:22:45 +0800

    Initial Commit

---
+++ lemoney/lemoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  lemoneyApp.swift
+//  lemoney
+//
+//  Created by T Krobot on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct lemoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
