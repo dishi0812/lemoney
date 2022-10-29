dde317a2f3a0b7fa75991c9fe77e20a4fb2e1f4c
dishita bharadia <dishita.bharadia@gmail.com>
2022-10-29 12:12:51 +0800

    added a sheet with picker and text field -Regina

---
+++ lemoney/CreateWishlistSheet.swift
@@ -0,0 +1,37 @@
+//
+//  CreateWishlistSheet.swift
+//  lemoney
+//
+//  Created by T Krobot on 29/10/22.
+//
+
+import SwiftUI
+
+struct CreateWishlistSheet: View {
+    @State var item = ""
+    @State var type = 1
+    var body: some View {
+        VStack {
+            Form {
+                Section {
+                    Picker("Type", selection: $type) {
+                        ForEach(1 ..< 3, content: { i in
+                            if (i == 1) {
+                                Text("Need")
+                            } else {
+                                Text("Want")
+                            }
+                        })
+                        .pickerStyle(.segmented)
+                    }
+                    TextField("Enter item here", text: $item)
+                }
+            }
+        }
+    }
+}
+struct CreateWishlistSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateWishlistSheet()
+    }
+}
