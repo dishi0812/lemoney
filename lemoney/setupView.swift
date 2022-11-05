d94312ccab3e078e6465b3d9349815d939571bb3
dishita bharadia <dishita.bharadia@gmail.com>
2022-11-05 11:58:33 +0800

    added setupView sheet and submit button

--- lemoney/setupView.swift
+++ lemoney/setupView.swift
@@ -1,39 +1,41 @@
 import SwiftUI
 
 struct setupView: View {
-    @State var categories: [Category]
+    @Binding var categories: [Category]
     @State var incomeText = ""
     @State var monthlyExpensesText = ""
     @State var budgetGoalsText = ""
+    
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Income: ", text: $incomeText)
-                    TextField("Monthly expenses: ", text: $monthlyExpensesText)
-                    TextField("Budget goals: ", text: $budgetGoalsText)
+                    TextField("Income", text: $incomeText)
+                    TextField("Monthly expenses", text: $monthlyExpensesText)
+                    TextField("Budget goals", text: $budgetGoalsText)
                 }
                 Section {
-                    Text("Transport:")
-                    Text("Food:")
-                    Text("Clothes:")
-                    Text("Entertainment:")
-                    Text("Stationery:")
+                    ForEach($categories) { $category in
+                        HStack {
+                            Text("\(category.name)")
+                            Spacer()
+                            Text("$")
+                            TextField("", value: $category.budget, formatter: NumberFormatter())
+                                .frame(width: 45, height: 30)
+                                .background(Color(.systemGray6))
+                                .fontWeight(.bold)
+                                .cornerRadius(8)
+                        }
+                    }
+                }
+                Button {
+                    
+                } label: {
+                    Text("Start saving money!")
                 }
             }
             .navigationTitle("Setup")
         }
-    }
-}
-
-struct setupView_Previews: PreviewProvider {
-    static var previews: some View {
-        setupView(categories: [
-            Category(name: "Transport", expenses: [], budget: 150.00, isStartingCategory: true),
-            Category(name: "Food", expenses: [], budget: 150.00, isStartingCategory: true),
-            Category(name: "Clothes", expenses: [], budget: 150.00, isStartingCategory: true),
-            Category(name: "Entertainment", expenses: [], budget: 150.00, isStartingCategory: true),
-            Category(name: "Stationery", expenses: [], budget: 150.00, isStartingCategory: true)
-        ])
     }
 }
