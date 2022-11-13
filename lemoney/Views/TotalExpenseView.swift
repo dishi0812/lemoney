6614921efda6975cc80f1814337fa6ce0e1d5aec
JX <jingxian.lau01@gmail.com>
2022-11-13 17:32:42 +0800

    Spendings in TotalExpenseView Works

--- lemoney/Views/TotalExpenseView.swift
+++ lemoney/Views/TotalExpenseView.swift
@@ -9,12 +9,14 @@ struct TotalExpenseView: View {
     
     @State var addExpenseSheetShown = false
     
-    @State var totalSpendings: Double
+    var totalSpendings: Double {
+        categories.reduce(0) { $0 + $1.spendings }
+    }
     @Binding var budgetGoal: Double
     @Binding var savingsGoal: Double
     @Binding var balance: Double
     
-    @State var allExpenses: [Expense] = []
+    var allExpenses: [Expense] { categories.reduce([]) {$0 + $1.expenses}.sorted(by: { $0.date.timeIntervalSince1970 > $1.date.timeIntervalSince1970 }) }
     
     var body: some View {
         VStack {
@@ -84,21 +86,14 @@ struct TotalExpenseView: View {
         .sheet(isPresented: $addExpenseSheetShown) {
             AddExpenseSheet(categoryIndex: 0, categories: $categories, budgetGoal: $budgetGoal, savingsGoal: $savingsGoal, balance: $balance)
         }
-//        .alert("Are you sure you want to delete this expense?", isPresented: $deleteAlertShown) {
-//            Button("Delete", role: .destructive) {
-//                let categoryIndex = categories.firstIndex(where: {$0.id == categoryId})!
-//                let value = categories[categoryIndex].expenses.first(where: { $0.id == expenseId })!.price
-//                balance += value
-//                totalSpendings -= value
-//                categories[categoryIndex].expenses = categories[categoryIndex].expenses.filter { $0.id != expenseId }
-//                allExpenses = categories.reduce([]) { $0 + $1.expenses }
-//                allExpenses = allExpenses.sorted(by: { $0.date.timeIntervalSince1970 > $1.date.timeIntervalSince1970 })
-//            }
-//            Button("Cancel", role: .cancel) {}
-//        }
-//        .onChange(of: categories) {
-//            allExpenses = categories.reduce([]) { $0 + $1.expenses }
-//            allExpenses = allExpenses.sorted(by: { $0.date.timeIntervalSince1970 > $1.date.timeIntervalSince1970 })
-//        }
+        .alert("Are you sure you want to delete this expense?", isPresented: $deleteAlertShown) {
+            Button("Delete", role: .destructive) {
+                let categoryIndex = categories.firstIndex(where: {$0.id == categoryId})!
+                let value = categories[categoryIndex].expenses.first(where: { $0.id == expenseId })!.price
+                balance += value
+                categories[categoryIndex].expenses = categories[categoryIndex].expenses.filter { $0.id != expenseId }
+            }
+            Button("Cancel", role: .cancel) {}
+        }
     }
 }
